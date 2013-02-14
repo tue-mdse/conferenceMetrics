b77487e8a2bed898a9f6e3f25e978b1f898b7e6e
Bogdan Vasilescu <vasilescu@gmail.com>
2013-02-14 13:39:41 +0100

    licenses

--- tools/python/unicodeMagic.py
+++ tools/python/unicodeMagic.py
@@ -1,3 +1,22 @@
+"""Copyright 2012-2013
+Eindhoven University of Technology (Bogdan Vasilescu and Alexander Serebrenik) and
+University of Mons (Tom Mens)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>."""
+
+"""Based on http://stackoverflow.com/questions/3085263/create-an-utf-8-csv-file-in-python"""
+
 import csv, codecs, cStringIO
 
 class UTF8Recoder:
--- tools/r/visualization.r
+++ tools/r/visualization.r
@@ -1,4 +1,20 @@
-
+# Copyright 2012-2013
+# Eindhoven University of Technology (Bogdan Vasilescu and Alexander Serebrenik) and
+# University of Mons (Tom Mens)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Lesser General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    
 root <- "../../"
 
 make_plot <- function(filename, metric, ylim, axis2, legendy, hline=FALSE){
